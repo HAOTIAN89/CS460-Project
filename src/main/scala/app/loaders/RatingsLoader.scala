a62c55f951af350a81fb04d128116ee15a039289
HAOTIAN89 <1285770300@qq.com>
2024-05-20 20:52:44 +0200

    finish the data loader

--- src/main/scala/app/loaders/RatingsLoader.scala
+++ src/main/scala/app/loaders/RatingsLoader.scala
@@ -18,5 +18,17 @@ class RatingsLoader(sc : SparkContext, path : String) extends Serializable {
    *
    * @return The RDD for the given ratings
    */
-  def load() : RDD[(Int, Int, Option[Double], Double, Int)] = ???
+  def load() : RDD[(Int, Int, Option[Double], Double, Int)] = {
+    val raw = sc.textFile(getClass.getResource(path).getPath)
+    // val raw = sc.textFile(path)
+    raw.map { line =>
+      val splitting = line.split("\\|")
+      val user_id = splitting(0).toInt
+      val movie_id = splitting(1).toInt
+      val old_rating = if (splitting.length > 4 && splitting(2).nonEmpty) Some(splitting(2).toDouble) else None // option
+      val new_rating = splitting(if (splitting.length == 4) 2 else 3).toDouble
+      val timestamp = splitting(if (splitting.length == 4) 3 else 4).toInt
+      (user_id, movie_id, old_rating, new_rating, timestamp)
+     }
+  }
 }

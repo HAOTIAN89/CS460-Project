4b710c60867aa9bbfc81c67c7908bfdd3f870275
HAOTIAN89 <1285770300@qq.com>
2024-05-24 13:51:29 +0200

    finish the task4.5

--- src/main/scala/app/recommender/Recommender.scala
+++ src/main/scala/app/recommender/Recommender.scala
@@ -22,10 +22,35 @@ class Recommender(sc: SparkContext, index: LSHIndex, ratings: RDD[(Int, Int, Opt
    * Returns the top K recommendations for movies similar to the List of genres
    * for userID using the BaseLinePredictor
    */
-  def recommendBaseline(userId: Int, genre: List[String], K: Int): List[(Int, Double)] = ???
+  def recommendBaseline(userId: Int, genre: List[String], K: Int): List[(Int, Double)] = {
+    val user_movie_ids = ratings.filter(_._1 == userId).map(_._2).collect().toSet
+    val genreRDD = sc.parallelize(List(genre))
+
+    // get movies from genre search and filter out movies the user has already rated for being efficient
+    val movie_recommend_base = nn_lookup.lookup(genreRDD).flatMap { case (_, movies) => movies }
+      .map { case (movie_id, _, _) => movie_id }
+      .filter { movie_id => !user_movie_ids.contains(movie_id)}
+      .collect().toList
+      .map { movie_id => (movie_id, baselinePredictor.predict(userId, movie_id)) }
+      .sortBy(-_._2).take(K)
+
+    movie_recommend_base
+  }
 
   /**
    * The same as recommendBaseline, but using the CollaborativeFiltering predictor
    */
-  def recommendCollaborative(userId: Int, genre: List[String], K: Int): List[(Int, Double)] = ???
+  def recommendCollaborative(userId: Int, genre: List[String], K: Int): List[(Int, Double)] = {
+    val user_movie_ids = ratings.filter(_._1 == userId).map(_._2).collect().toSet
+    val genreRDD = sc.parallelize(List(genre))
+
+    val movie_recommend_base = nn_lookup.lookup(genreRDD).flatMap { case (_, movies) => movies }
+      .map { case (movie_id, _, _) => movie_id }
+      .filter { movie_id => !user_movie_ids.contains(movie_id) }
+      .collect().toList
+      .map { movie_id => (movie_id, collaborativePredictor.predict(userId, movie_id)) }
+      .sortBy(-_._2).take(K)
+
+    movie_recommend_base
+  }
 }

55ffbf752f93793b3943df7610307312ce2cf51f
HAOTIAN89 <1285770300@qq.com>
2024-05-24 11:46:46 +0200

    finish the task4.4

--- src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
+++ src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
@@ -11,10 +11,27 @@ class CollaborativeFiltering(rank: Int,
 
   // NOTE: set the parameters according to the project description to get reproducible (deterministic) results.
   private val maxIterations = 20
-  private var model = null
+  private var model: MatrixFactorizationModel = null
 
-  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = ???
+  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = {
+    // convert ratingsRDD to a format suitable for ALS
+    val train_data = ratingsRDD.map {
+      case (user_id, movie_id, _, new_rating, _) => Rating(user_id, movie_id, new_rating)
+    }
 
-  def predict(userId: Int, movieId: Int): Double = ???
+    // train the model
+    model = ALS.train(ratings = train_data,
+      rank = rank,
+      iterations = maxIterations,
+      lambda = regularizationParameter,
+      blocks = n_parallel,
+      seed = seed)
+  }
+
+  def predict(userId: Int, movieId: Int): Double = {
+    val predict_result = model.predict(userId, movieId)
+    
+    predict_result
+  }
 
 }
